7a1693d6c47d0a7a3b0594f554f1e9d9e946c234
Amr Taman <96953623+AmrTaman@users.noreply.github.com>
2023-10-24 00:41:01 +0300

    Create 0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include"sort.h"
+/**
+ * bubble_sort - sorts with bubble algorithm
+ * @array: array if integers
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t x = 0;
+	int temp, count = 1;
+
+	while (count != 0)
+	{
+		count = 0;
+		for (x = 0; x < size - 1; x++)
+		{
+			if (array[x] > array[x + 1])
+			{
+				temp = array[x];
+				array[x] = array[x + 1];
+				array[x + 1] = temp;
+				count++;
+				print_array(array, size);
+			}
+		}
+	}
+	return;
+}
